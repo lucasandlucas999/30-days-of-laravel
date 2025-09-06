65f5ff25fc8ea45f96f056e1c4ac610c452d988e
Lucas <contactolucasacuna@gmail.com>
2025-09-06 09:22:41 -0300

    Indentando texto...

--- resources/views/lucas.blade.php
+++ resources/views/lucas.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
-   <x-slot:heading>Sobre Lucas</x-slot:heading>
-
+    <x-slot:heading>Sobre Lucas</x-slot:heading>
     <h1>Welcome to the Lucas Page</h1>
     <p>This is the content of the Lucas page.</p>
 </x-layout>
