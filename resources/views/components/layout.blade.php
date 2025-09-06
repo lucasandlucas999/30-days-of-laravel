3bf4be65dd1474a7d1f074d22780001c9be1fbcb
Lucas <contactolucasacuna@gmail.com>
2025-09-06 08:59:19 -0300

    Creando componente para NAV, con clases y renderizado condicional

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,14 +21,14 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                                <a href="/"
-                                    class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">Home</a>
-                                <a href="/about"
-                                    class="{{ request()->is('about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">About</a>
-                                <a href="/contact"
-                                    class="{{ request()->is('contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">Contact</a>
 
+                                {{-- no es necesario pasar la url con '/', el componente lo renderiza sin ella, la unica excepcion es para acceder a '/' --}}
+
+                                <x-nav-link href="/">Home</x-nav-link>
+                                <x-nav-link href="about">About</x-nav-link>
+                                <x-nav-link href="contact">Contact</x-nav-link>
+                                <x-nav-link href="lucas">Lucas</x-nav-link>
+                                
                             </div>
                         </div>
                     </div>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,3 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+<a href="/{{ ltrim($attributes['href'], '/') }}"
+    class="{{ request()->is($attributes['href']) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">
+    {{ $slot }} </a>
