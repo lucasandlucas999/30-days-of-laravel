a47448f8e0330120bf09ea88e6b7066d7fabf048
Lucas <contactolucasacuna@gmail.com>
2025-09-09 15:47:29 -0300

    Añadiendo migration, class y factory de Employer para probar un foreign key, final de capitulo 10 del curso

--- database/migrations/2025_09_09_183743_create_employers_table.php
+++ database/migrations/2025_09_09_183743_create_employers_table.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Employer;
 
 return new class extends Migration
 {
@@ -13,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('employers', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Employer::class);
-            $table->string('title');
-            $table->string('salary');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('employers');
     }
 };
